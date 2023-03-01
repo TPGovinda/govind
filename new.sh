e6a2660c95236f9ac50804a501d8e3a09f4f8bbc
TPGovinda <125262933+TPGovinda@users.noreply.github.com>
2023-03-01 12:56:45 +0530

    Create new.sh

---
+++ new.sh
@@ -0,0 +1,8 @@
+- name: Run a multi-line script
+        run: | 
+          echo hello govind 
+          ls -a
+          echo who is working on my script ?
+          whoami
+          df
+          env
